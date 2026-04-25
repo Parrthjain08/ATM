ba090597454ad74c8e23b25d0d213677e0446436
Parrth Jain <2510001423@cgcuniversity.org>
2026-04-25 19:00:00 +0530

    Create Choice_selection.py

---
+++ Choice_selection.py
@@ -0,0 +1,27 @@
+def Atm():
+    pin()
+    while True:
+        print("\nPlease select an option:")
+        print("1. Withdraw")
+        print("2. Deposit")
+        print("3. Check Balance")
+        print("4. Statement")
+        print("5. Exit")
+
+        choice = input("Enter your choice (1-5): ")
+
+        if choice == '1':
+            Withdraw()
+        elif choice == '2':
+            Deposit()
+        elif choice == '3':
+            Check_balance()
+        elif choice == '4':
+            Statement()
+        elif choice == '5':
+            print("Thank you for using the ATM. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+Atm()
