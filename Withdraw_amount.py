dd145d9b848a3d7d746d92586d30689e465cf59e
Parrth Jain <2510001423@cgcuniversity.org>
2026-04-25 18:54:49 +0530

    Create Withdraw_amount.py

---
+++ Withdraw_amount.py
@@ -0,0 +1,10 @@
+def Withdraw():
+    global Total_money
+    global money_withdraw
+    money_withdraw = float(input("Enter the amount to withdraw: "))
+    if Total_money >= money_withdraw and money_withdraw > 0:
+        print(f"You have withdrawn {money_withdraw:.2f}")
+        Total_money = Total_money - money_withdraw
+        print(f"Your new balance is {Total_money:.2f}")
+    else:
+        print("Invalid amount. Please enter a positive value.")
